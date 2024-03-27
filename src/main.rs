027c1e36fe565eaaa46366c18c3b7c3aef042a35
lbuckalew <lwb0003@auburn.edu>
2024-03-26 20:15:10 -0500

    sandworm can grow, hit itself, and hit walls

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 extern crate sdl2;
 
-use std::ops::Add;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
 use sdl2::rect::Rect;
 use sdl2::render::WindowCanvas;
 use sdl2::video::Window;
+use std::ops::Add;
 use std::time::Duration;
 
 // Grid units (how many sandworm segments per x or y)
@@ -18,11 +18,20 @@ const SANDWORM_SEGMENT_PX: u32 = 20;
 const WINDOW_SIZE_X: u32 = SPICEFIELD_SIZE_X * SANDWORM_SEGMENT_PX;
 const WINDOW_SIZE_Y: u32 = SPICEFIELD_SIZE_Y * SANDWORM_SEGMENT_PX;
 
-pub enum GameState {Playing, Paused}
+pub enum GameState {
+    Playing,
+    Paused,
+    Over,
+}
 
-pub enum WormDirection {Up, Right, Down, Left}
+pub enum WormDirection {
+    Up,
+    Right,
+    Down,
+    Left,
+}
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, PartialEq)]
 pub struct Coord {
     pub x: i32,
     pub y: i32,
@@ -48,9 +57,13 @@ pub struct GameContext {
 impl GameContext {
     pub fn new() -> GameContext {
         GameContext {
-            sandworm: vec![Coord{x:3, y:1}, Coord{x:2, y:1}, Coord{x:1, y:1}],
+            sandworm: vec![
+                Coord { x: 3, y: 1 },
+                Coord { x: 2, y: 1 },
+                Coord { x: 1, y: 1 },
+            ],
             sandworm_dir: WormDirection::Right,
-            sarduakar_invader: Coord{x:3, y:3},
+            sarduakar_invader: Coord { x: 3, y: 3 },
             current_state: GameState::Paused,
         }
     }
@@ -62,17 +75,33 @@ impl GameContext {
 
         let sandworm_head = self.sandworm.first().unwrap();
         let next_sandworm_head = match self.sandworm_dir {
-            WormDirection::Up => *sandworm_head + Coord{x:0, y:-1},
-            WormDirection::Right => *sandworm_head + Coord{x:1, y:0},
-            WormDirection::Down => *sandworm_head + Coord{x:0, y:1},
-            WormDirection::Left => *sandworm_head + Coord{x:-1, y:0},
+            WormDirection::Up => *sandworm_head + Coord { x: 0, y: -1 },
+            WormDirection::Right => *sandworm_head + Coord { x: 1, y: 0 },
+            WormDirection::Down => *sandworm_head + Coord { x: 0, y: 1 },
+            WormDirection::Left => *sandworm_head + Coord { x: -1, y: 0 },
         };
-        // Remove tail segment
-        self.sandworm.pop();
-        // Add new head
-        self.sandworm.reverse();
-        self.sandworm.push(next_sandworm_head);
-        self.sandworm.reverse();
+
+        if !self.is_in_bounds(next_sandworm_head) {
+            self.current_state = GameState::Over;
+        } else if self.sandworm.contains(&next_sandworm_head) {
+            self.current_state = GameState::Over;
+        } else if next_sandworm_head == self.sarduakar_invader {
+            // Remove tail segment
+            self.sandworm.pop();
+            // Add new head
+            self.sandworm.reverse();
+            self.sandworm.push(next_sandworm_head);
+            self.sandworm.reverse();
+            // Add new tail
+            self.grow_worm();
+        } else {
+            // Remove tail segment
+            self.sandworm.pop();
+            // Add new head
+            self.sandworm.reverse();
+            self.sandworm.push(next_sandworm_head);
+            self.sandworm.reverse();
+        }
     }
 
     pub fn move_up(&mut self) {
@@ -80,6 +109,7 @@ impl GameContext {
     }
 
     pub fn move_down(&mut self) {
+        println!("Move down!");
         self.sandworm_dir = WormDirection::Down;
     }
 
@@ -95,15 +125,58 @@ impl GameContext {
         self.current_state = match self.current_state {
             GameState::Paused => GameState::Playing,
             GameState::Playing => GameState::Paused,
+            GameState::Over => GameState::Over,
         }
+    }
+
+    // See if a candidate Coord is in bounds of the game
+    fn is_in_bounds(&self, c: Coord) -> bool {
+        c.x > 0 && c.x < SPICEFIELD_SIZE_X as i32 && c.y > 0 && c.y < SPICEFIELD_SIZE_Y as i32
+    }
+
+    // Determine which way to grow the sandworm tail and add a segment
+    fn grow_worm(&mut self) {
+        // Find direction to grow
+        let s0 = &self.sandworm[self.sandworm.len() - 1];
+        let s1 = &self.sandworm[self.sandworm.len() - 2];
+        let grow_dir = if s1.x == s0.x - 1 {
+            WormDirection::Right
+        } else if s1.x == s0.x + 1 {
+            WormDirection::Left
+        } else if s1.y == s0.y - 1 {
+            WormDirection::Down
+        } else {
+            WormDirection::Up
+        };
+
+        self.sandworm.push(match grow_dir {
+            WormDirection::Down => Coord {
+                x: s0.x,
+                y: s0.y + 1,
+            },
+            WormDirection::Up => Coord {
+                x: s0.x,
+                y: s0.y - 1,
+            },
+            WormDirection::Left => Coord {
+                x: s0.x - 1,
+                y: s0.y,
+            },
+            WormDirection::Right => Coord {
+                x: s0.x + 1,
+                y: s0.y,
+            },
+        });
     }
 }
 
-pub struct Renderer {canvas: WindowCanvas}
+pub struct Renderer {
+    canvas: WindowCanvas,
+}
 impl Renderer {
     pub fn new(window: Window) -> Result<Renderer, String> {
         let canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
-        Ok(Renderer{canvas})
+        Ok(Renderer { canvas })
     }
 
     fn draw_segment(&mut self, coord: &Coord) -> Result<(), String> {
@@ -113,7 +186,7 @@ impl Renderer {
             SANDWORM_SEGMENT_PX,
             SANDWORM_SEGMENT_PX,
         ))?;
-        
+
         Ok(())
     }
 
@@ -121,6 +194,7 @@ impl Renderer {
         let color = match ctx.current_state {
             GameState::Paused => Color::RGB(30, 30, 30),
             GameState::Playing => Color::RGB(76, 31, 22),
+            GameState::Over => Color::RGB(0, 0, 0),
         };
         self.canvas.set_draw_color(color);
         self.canvas.clear();
@@ -154,11 +228,7 @@ pub fn main() -> Result<(), String> {
     let video_subsystem = sdl_context.video()?;
 
     let window = video_subsystem
-        .window(
-            "~~~~~ SANDWORM ~~~~~", 
-            WINDOW_SIZE_X, 
-            WINDOW_SIZE_Y,
-        )
+        .window("~~~~~ SANDWORM ~~~~~", WINDOW_SIZE_X, WINDOW_SIZE_Y)
         .position_centered()
         .opengl()
         .build()
@@ -171,24 +241,34 @@ pub fn main() -> Result<(), String> {
 
     let mut frames = 0;
     'running: loop {
+        match ctx.current_state {
+            GameState::Over => {
+                ::std::thread::sleep(Duration::new(10, 0));
+                break 'running;
+            }
+            _ => (),
+        }
+
         for event in event_pump.poll_iter() {
             match event {
-                Event::Quit {..} => break 'running,
-                Event::KeyDown {keycode: Some(keycode), ..} => {
-                    match keycode {
-                        Keycode::W => ctx.move_up(),
-                        Keycode::D => ctx.move_right(),
-                        Keycode::S => ctx.move_down(),
-                        Keycode::A => ctx.move_left(),
-                        Keycode::Escape => ctx.toggle_pause(),
-                        _ => {},
-                    }
+                Event::Quit { .. } => break 'running,
+                Event::KeyDown {
+                    keycode: Some(keycode),
+                    ..
+                } => match keycode {
+                    Keycode::W => ctx.move_up(),
+                    Keycode::D => ctx.move_right(),
+                    Keycode::S => ctx.move_down(),
+                    Keycode::A => ctx.move_left(),
+                    Keycode::Escape => ctx.toggle_pause(),
+                    _ => {}
                 },
                 _ => {}
             }
         }
 
         if frames % 10 == 0 {
+            println!("update state");
             ctx.update_state();
             frames = 0;
         }
